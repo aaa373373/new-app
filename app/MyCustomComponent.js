8a6474995ac5394ef5e48ed4c902a972714131e1
Awura Abena Adjei <adjeia29@yahoo.com>
2023-09-29 20:44:35 -0400

    created a new app

---
+++ app/MyCustomComponent.js
@@ -0,0 +1,13 @@
+// MyCustomComponent.js
+import React from 'react';
+
+const MyCustomComponent = () => {
+  return (
+    <div>
+      <h2>This is My Custom Component</h2>
+      <p>It can include any content you want.</p>
+    </div>
+  );
+};
+
+export default MyCustomComponent;
---
+++ app/index.js
@@ -0,0 +1,12 @@
+// pages/index.js
+import React from 'react';
+import MyCustomComponent from '../components/MyCustomComponent'; // Update the path as needed
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello, Next.js!</h1>
+      <MyCustomComponent />
+    </div>
+  );
+}
